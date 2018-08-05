b1a3d293db608079caba59b46961f9009a8abf0d
monkataaa <monkataaa@abv.bg>
2018-08-05 15:26:33 +0300

    upload all files

---
+++ 3.Endurance Rally/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Endurance_Rally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> participants = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<decimal> zones = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(decimal.Parse).ToList();
+            List<long> originalCheckpoints = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+
+            foreach (var driver in participants)
+            {
+                var checkpoints = originalCheckpoints.ToList();
+                bool notFinished = false;
+                int lastZone = 0;
+                decimal fuel = (decimal)driver[0];
+
+                for (int i = 0; i < zones.Count; i++)
+                {
+                    if (checkpoints.Count != 0 && checkpoints.Contains(i) && i == checkpoints[0])
+                    {
+                        fuel += zones[i];
+                   //     checkpoints.RemoveAt(0);
+                    }
+                    else
+                    {
+                        fuel -= zones[i];
+                    }
+
+                    if (fuel <= 0)
+                    {
+                        notFinished = true;
+                        lastZone = i;
+                        break;
+                    }
+
+     
+                }
+                if (notFinished)
+                {
+                    Console.WriteLine($"{driver} - reached {lastZone}");  
+                }
+                else
+                {
+                    Console.WriteLine($"{driver} - fuel left {fuel:F2}");
+                }
+             
+            }
+
+        }
+    }
+}
---
+++ 4 – Cubic’s Messages/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _4___Cubic_s_Messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                string inputString = Console.ReadLine();
+                if (inputString == "Over!")
+                {
+                    break;
+                }
+                int num = int.Parse(Console.ReadLine());
+
+                string pattern = @"^(\d+)([a-zA-Z]{" + num + @"})([^a-zA-Z]*)$";
+                MatchCollection matches = Regex.Matches(inputString, pattern);
+                List<int> numbers = new List<int>();
+
+                foreach (Match match in matches)
+                {
+                    string code = match.Groups[2].ToString();
+                    var numGroups = match.Groups[1].ToString() + match.Groups[3];
+                    var onlyDigitsChars = numGroups.Where(Char.IsDigit).ToArray();
+
+                    List<char> codesList = new List<char>();
+                    foreach (var index in onlyDigitsChars)
+                    {
+                        int indexNum = int.Parse(index.ToString());
+                        if (indexNum >= 0 && indexNum < code.Length)
+                        {
+                            codesList.Add(code[indexNum]);
+                        }
+                        else
+                        {
+                            codesList.Add(' ');
+                        }
+                    }
+                    Console.WriteLine($"{code} == {string.Join("", codesList)}");
+                }
+
+
+            }
+
+
+        }
+    }
+}
